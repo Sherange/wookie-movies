a12b69a74a2cf7f24eff2567d3c868afce779d42
Sherange <sherangef@gmail.com>
2021-11-14 22:38:44 +0530

    update unit tests

--- src/screens/HomeScreen/__tests__/HomeScreen-test.js
+++ src/screens/HomeScreen/__tests__/HomeScreen-test.js
@@ -1,13 +1,25 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+import {store} from '../../../redux/store';
 import HomeScreen from '../index';
 
 it('renders correctly', () => {
-  renderer.create(<HomeScreen />);
+  renderer.create(
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>,
+  );
 });
 
 test('Home snapshot', () => {
-  const snapshot = renderer.create(<HomeScreen />).toJSON();
+  const snapshot = renderer
+    .create(
+      <Provider store={store}>
+        <HomeScreen />
+      </Provider>,
+    )
+    .toJSON();
   expect(snapshot).toMatchSnapshot();
 });
